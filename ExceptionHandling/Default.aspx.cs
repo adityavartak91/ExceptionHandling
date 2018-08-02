094f5209dde1b9c6688f1e26a2f96df1b67b2342
adityavartak91 <vartakaditya16@gmail.com>
2018-08-02 12:59:51 +0530

    Initial commit

---
+++ ExceptionHandling/Default.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.Practices.EnterpriseLibrary.ExceptionHandling;
+
+namespace ExceptionHandling
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            BaseException();
+            DivideByZeroException();
+        }
+
+
+        public  void BaseException()
+        {
+            try
+            {
+                string x = null;
+                int a = 10;
+
+                Response.Write(int.Parse(x) + a);
+            }
+            catch (Exception ex)
+            {
+                ExceptionHandler obj = new ExceptionHandler();
+                string errormsg = obj.HandleException(ex);
+                Response.Write(errormsg);
+            }
+        }
+
+        public static void DivideByZeroException()
+        {
+            try
+            {
+                int x = 5;
+                int y = 0;
+                int z = x / y;
+            }
+            catch (DivideByZeroException ex)
+            {
+                ExceptionHandler obj = new ExceptionHandler();
+                string errormsg = obj.HandleException(ex);
+            }
+        }
+
+        public static void FileNotFoundException()
+        {
+            try
+            {
+                // Read in non-existent file.  
+                using (System.IO.StreamReader reader = new System.IO.StreamReader("TextFile1.txt"))
+                {
+                    reader.ReadToEnd();
+                }
+            }
+            catch (System.IO.FileNotFoundException ex)
+            {
+                ExceptionHandler obj = new ExceptionHandler();
+                string errormsg = obj.HandleException(ex);
+            }
+        }
+    }
+}
---
+++ ExceptionHandling/ExceptionHandler.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Practices.EnterpriseLibrary.ExceptionHandling;
+using Microsoft.Practices.EnterpriseLibrary.Logging;
+
+
+namespace ExceptionHandling
+{
+    public class ExceptionHandler
+    {
+        #region Static Variables  
+
+        private static LogWriter logWriter;
+        private static readonly ExceptionPolicyFactory _exceptionPolicyFactory;
+        private static readonly ExceptionManager _exceptionManager;
+
+        #endregion
+
+        #region Constructor  
+
+        static ExceptionHandler()
+        {
+            logWriter = new LogWriterFactory().Create();
+            Logger.SetLogWriter(logWriter, false);
+
+            _exceptionPolicyFactory = new ExceptionPolicyFactory();
+            _exceptionManager = _exceptionPolicyFactory.CreateManager();
+        }
+
+        #endregion
+
+        #region Static Methods  
+
+        /// <summary>  
+        /// Performs the handling of an application towards the configured policy.         
+        /// <param name="ex">Exception to handle.</param>  
+        public string HandleException(Exception ex)
+        {
+            Exception exceptionToThrow = null;
+            string errmessage = "";
+            //The HandleException method will return true if the exception should be (re-)thrown.  
+            if (_exceptionManager.HandleException(ex, "DivideByZero Exception Handler", out exceptionToThrow))
+            {
+                if (exceptionToThrow == null)
+                {
+                    errmessage = ex.Message;
+                }
+                else
+                {
+                    errmessage = exceptionToThrow.Message;
+                }
+            }
+            return errmessage;
+        }
+
+        #endregion
+    }
+}
